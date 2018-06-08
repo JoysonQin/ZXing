3d5129f2bb9b7f686eed73a2c4ec9b1bea2a563a
259440 <259440@sh.lianjia.com>
2018-06-08 14:27:49 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/ZXingApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZXingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZXingApplication.class, args);
+	}
+}
